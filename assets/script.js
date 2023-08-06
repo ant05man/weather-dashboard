a9e800b512ed814b784ffec9192417b9bc45c01a
Anthony Littlejohn <ant05man@yahoo.com>
2023-08-06 16:37:13 -0400

    added link to api and storing input history

--- assets/script.js
+++ assets/script.js
@@ -42,3 +42,70 @@ $(document).ready(function () {
       weatherForecast($(this).text());
     });
 });
+
+$.ajax({
+  type: "GET",
+  url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=9f112416334ce37769e5c8683b218a0d",
+
+
+}).then(function (data) {
+  //if index of search value does not exist
+  if (history.indexOf(searchTerm) === -1) {
+    //push searchValue to history array
+    history.push(searchTerm);
+    //places item pushed into local storage
+    localStorage.setItem("history", JSON.stringify(history));
+    createRow(searchTerm);
+  }
+  // clears out old content
+  $("#today").empty();
+
+//
+  var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
+  var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+
+
+  var card = $("<div>").addClass("card");
+  var cardBody = $("<div>").addClass("card-body");
+  var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
+  var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + " %");
+  var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " K");
+  console.log(data)
+  var lon = data.coord.lon;
+  var lat = data.coord.lat;
+  
+  $.ajax({
+    type: "GET",
+    url: "https://api.openweathermap.org/data/2.5/uvi?appid=9f112416334ce37769e5c8683b218a0d&lat=" + lat + "&lon=" + lon,
+
+
+  }).then(function (response) {
+    console.log(response);
+
+    var uvColor;
+    var uvResponse = response.value;
+    var uvIndex = $("<p>").addClass("card-text").text("UV Index: ");
+    var btn = $("<span>").addClass("btn btn-sm").text(uvResponse);
+
+
+    if (uvResponse < 3) {
+      btn.addClass("btn-success");
+    } else if (uvResponse < 7) {
+      btn.addClass("btn-warning");
+    } else {
+      btn.addClass("btn-danger");
+    }
+
+    cardBody.append(uvIndex);
+    $("#today .card-body").append(uvIndex.append(btn));
+
+  });
+
+  // merge and add to page
+  title.append(img);
+  cardBody.append(title, temp, humid, wind);
+  card.append(cardBody);
+  $("#today").append(card);
+  console.log(data);
+});
+}
